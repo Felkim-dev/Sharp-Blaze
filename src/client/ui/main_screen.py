61e07b57530d76ba585a04a914f1fb6a1ae42e12
Felipe Quilumbango <fipojaq2005@gmail.com>
2026-03-25 01:31:41 -0500

    Design and Structure of Main Screen Finished

--- src/client/ui/main_screen.py
+++ src/client/ui/main_screen.py
@@ -5,21 +5,40 @@ from ui.component import Button
 class MainScreen:
     def __init__(self, screen):
         self.screen = screen
+
+        # Colors
         self.MAINDARK = (19,23,34)
-        # Instanciar tus componentes según la HU-01
-        # self.ip_input = InputBox(x, y, width, height, "IP Address")
-        # self.id_input = InputBox(x, y, width, height, "Username")
-        self.btn_host = Button((500,360),(0,212,255),(350,50), "Host Game",24)
-        self.btn_join = Button((500, 300), (0, 212, 255), (350, 50), "Join Game", 24)
-        # self.btn_join = Button(x, y, "Join Game")
+        self.WHITE = (255, 255, 255)
+        LIGHT_BLUE = (0,212,255)
+        BLACK = (0, 0, 0)
+        RED = (204, 5, 35)
+
+        # Button and Text Size
+        BUTTON_WH = (350, 50)
+        TEXT_SIZE = 24
+
+        self.btn_host = Button((480, 310), BUTTON_WH, LIGHT_BLUE,"Host Game", BLACK, TEXT_SIZE)
+        self.btn_join = Button((480, 370), BUTTON_WH, LIGHT_BLUE, "Join Game", BLACK, TEXT_SIZE)
+        self.btn_bot = Button((480, 430), BUTTON_WH, LIGHT_BLUE, "Bot Match", BLACK, TEXT_SIZE)
+        self.btn_options = Button((480, 490), BUTTON_WH, LIGHT_BLUE, "Options", BLACK, TEXT_SIZE)
+        self.btn_exit = Button((480, 550), BUTTON_WH, RED, "Exit", BLACK, TEXT_SIZE)
 
     def handle_events(self, events):
         # Aquí le pasas los eventos (clics, teclado) a tus inputs y botones
         pass
 
     def draw(self):
+        # SCREEN DRAW
         self.screen.fill((self.MAINDARK))  # Fondo oscuro
-        # self.ip_input.draw(self.screen)
-        # self.id_input.draw(self.screen)
+
+        # BUTTONS DRAW
         self.btn_host.draw(self.screen)
         self.btn_join.draw(self.screen)
+        self.btn_bot.draw(self.screen)
+        self.btn_options.draw(self.screen)
+        self.btn_exit.draw(self.screen)
+
+        # TEXT DRAW
+        text_font = pygame.font.Font(r"C:\Users\felip\OneDrive\Escritorio\SEPTIMO_SEMESTRE\Sharp-Blaze\assets\Anton-Regular.ttf",80)
+        text_surface = text_font.render("SHARP BLAZE", True, self.WHITE)
+        self.screen.blit(text_surface,(460,140))
