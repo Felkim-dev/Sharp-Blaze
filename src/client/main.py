86fe99c41de332a1fabc670527fb22633a1cb0ed
Felipe Quilumbango <fipojaq2005@gmail.com>
2026-03-25 11:03:33 -0500

    Class Game Implemented/ Screen Manager implemented

--- src/client/main.py
+++ src/client/main.py
@@ -1,37 +1,53 @@
 # main.py
 import pygame
 import sys
-from ui.connection_screen import ConnectionScreen
+
+#IMPORT OF SCREENS
 from ui.main_screen import MainScreen
+from ui.host_screen import HostScreen
+from ui.join_screen import JoinScreen
 
-WIDTH=1280
-HEIGHT=720
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Sharp Blaze")
-    clock = pygame.time.Clock()
-    # Cargamos la pantalla inicial
-    current_screen = MainScreen(screen)
-    running = True
-    while running:
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                running = False
-
-        # 1. Procesar eventos (clics, teclas)
-        current_screen.handle_events(events)
-        # 2. Renderizar gráficos
-        current_screen.draw()
-
-        pygame.display.flip()
-        clock.tick(30)  # Limitamos a 30 FPS según el Project Charter
-
-    pygame.quit()
-    sys.exit()
-
+class GAME:
+    
+    def __init__(self):
+        
+        WIDTH=1280
+        HEIGHT=720
+        
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Sharp Blaze")
+        self.clock = pygame.time.Clock()
+        
+        self.screens = {
+            "MAIN": MainScreen(self,self.screen),
+            # "HOST": HostScreen(self,self.screen),
+            # "JOIN": JoinScreen(self,self.screen),
+                        }
+        
+        self.current_screen = self.screens["MAIN"]
+        
+    def change_screen(self, screen_name):
+        if screen_name in self.screens:
+            self.current_screen = self.screens[screen_name]
+        else:
+         #Borrar
+            print("ERROR: Screen Does not Exists")
+    
+    def run(self):
+        while True:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.current_screen.handle_events(events)
+            self.current_screen.draw()
+            
+            pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == "__main__":
-    main()
+    game = GAME()
+    game.run()
