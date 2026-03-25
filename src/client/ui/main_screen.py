2540babcfce87b654188abd8f4dd9d7c0c8e936d
Felipe Quilumbango <fipojaq2005@gmail.com>
2026-03-25 01:45:41 -0500

    Window Name Changed and Dinamyc Layouts Implemented

--- src/client/ui/main_screen.py
+++ src/client/ui/main_screen.py
@@ -16,12 +16,20 @@ class MainScreen:
         # Button and Text Size
         BUTTON_WH = (350, 50)
         TEXT_SIZE = 24
+        
+        #Calculate Pos
+        width_button = BUTTON_WH[0]
+        center_x = self.screen.get_rect().centerx - (width_button //2)
+        
+        init_y = 310
+        separation_y = 60
 
-        self.btn_host = Button((480, 310), BUTTON_WH, LIGHT_BLUE,"Host Game", BLACK, TEXT_SIZE)
-        self.btn_join = Button((480, 370), BUTTON_WH, LIGHT_BLUE, "Join Game", BLACK, TEXT_SIZE)
-        self.btn_bot = Button((480, 430), BUTTON_WH, LIGHT_BLUE, "Bot Match", BLACK, TEXT_SIZE)
-        self.btn_options = Button((480, 490), BUTTON_WH, LIGHT_BLUE, "Options", BLACK, TEXT_SIZE)
-        self.btn_exit = Button((480, 550), BUTTON_WH, RED, "Exit", BLACK, TEXT_SIZE)
+        #Buttons declarations
+        self.btn_host = Button((center_x, init_y + separation_y * 0), BUTTON_WH, LIGHT_BLUE,"Host Game", BLACK, TEXT_SIZE)
+        self.btn_join = Button((center_x, init_y + separation_y * 1), BUTTON_WH, LIGHT_BLUE, "Join Game", BLACK, TEXT_SIZE)
+        self.btn_bot = Button((center_x, init_y + separation_y * 2), BUTTON_WH, LIGHT_BLUE, "Bot Match", BLACK, TEXT_SIZE)
+        self.btn_options = Button((center_x, init_y + separation_y * 3), BUTTON_WH, LIGHT_BLUE, "Options", BLACK, TEXT_SIZE)
+        self.btn_exit = Button((center_x, init_y + separation_y * 4), BUTTON_WH, RED, "Exit", BLACK, TEXT_SIZE)
 
     def handle_events(self, events):
         # Aquí le pasas los eventos (clics, teclado) a tus inputs y botones
@@ -39,6 +47,7 @@ class MainScreen:
         self.btn_exit.draw(self.screen)
 
         # TEXT DRAW
-        text_font = pygame.font.Font(r"C:\Users\felip\OneDrive\Escritorio\SEPTIMO_SEMESTRE\Sharp-Blaze\assets\Anton-Regular.ttf",80)
+        text_font = pygame.font.Font(r"C:\Users\felip\OneDrive\Escritorio\SEPTIMO_SEMESTRE\Sharp-Blaze\assets\Anton-Regular.ttf",100)
         text_surface = text_font.render("SHARP BLAZE", True, self.WHITE)
-        self.screen.blit(text_surface,(460,140))
+        title_rect = text_surface.get_rect(center = (self.screen.get_rect().centerx, 180))
+        self.screen.blit(text_surface,title_rect)
