a784bcd9a9521f9123358186a4463a2a123a9622
Felipe Quilumbango <fipojaq2005@gmail.com>
2026-03-25 10:27:29 -0500

    Check Hover implemented for Button, Color copy and white implemented

--- src/client/ui/component.py
+++ src/client/ui/component.py
@@ -15,10 +15,12 @@ class Button:
         # Characteristics
         self.Position = Position
         self.ButtonColor = ButtonColor
+        self.ButtonColor_copy = ButtonColor
         self.RectangleDimension = RectangleDimension
         self.text_string = Text
         self.button_font = pygame.font.Font(r"C:\Users\felip\OneDrive\Escritorio\SEPTIMO_SEMESTRE\Sharp-Blaze\assets\IntroRust.otf", TextSize)
         self.corners_radius = 7
+        self.WHITE = (255, 255, 255)
         
         # Figures Instantiation
         self.button_rectangle = pygame.Rect(Position,(RectangleDimension))
@@ -27,11 +29,17 @@ class Button:
         # Text Rec Center
         self.text_rect = self.text_surface.get_rect()
         self.text_rect.center = self.button_rectangle.center
-
+    
+    def check_hover(self,mouse_pos):
+        
+        if self.button_rectangle.collidepoint(mouse_pos):
+            self.ButtonColor = self.WHITE
+        else: 
+            self.ButtonColor = self.ButtonColor_copy
+    
     def draw(self,screen):
-
+        
         pygame.draw.rect(screen,self.ButtonColor,self.button_rectangle, border_radius= self.corners_radius)
         screen.blit(self.text_surface, self.text_rect)
-
 class InputBox:
     pass
