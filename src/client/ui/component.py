2bdf3a17665816cbe36993343023bfaa48d175a1
Felipe Quilumbango <fipojaq2005@gmail.com>
2026-03-25 01:30:50 -0500

    Add Variables to add legibility

--- src/client/ui/component.py
+++ src/client/ui/component.py
@@ -1,28 +1,37 @@
 import pygame
 
 class Button:
-    def __init__(self, Position:tuple,ButtonColor:tuple, RectangleDimension:tuple, Text:str, TextSize:int):
-        
-        #Characteristics
+
+    def __init__(
+        self,
+        Position: tuple,
+        RectangleDimension: tuple,
+        ButtonColor: tuple,
+        Text: str,
+        TextColor:tuple,
+        TextSize: int,
+    ):
+
+        # Characteristics
         self.Position = Position
         self.ButtonColor = ButtonColor
         self.RectangleDimension = RectangleDimension
         self.text_string = Text
         self.button_font = pygame.font.Font(r"C:\Users\felip\OneDrive\Escritorio\SEPTIMO_SEMESTRE\Sharp-Blaze\assets\IntroRust.otf", TextSize)
+        self.corners_radius = 7
         
-        #Figures Instantiation
+        # Figures Instantiation
         self.button_rectangle = pygame.Rect(Position,(RectangleDimension))
-        self.text_surface = self.button_font.render(self.text_string,True,(0, 0, 0))
-        
+        self.text_surface = self.button_font.render(Text,True,TextColor)
+
         # Text Rec Center
         self.text_rect = self.text_surface.get_rect()
         self.text_rect.center = self.button_rectangle.center
-        
+
     def draw(self,screen):
-        
-        pygame.draw.rect(screen,self.ButtonColor,self.button_rectangle,border_radius=7)
+
+        pygame.draw.rect(screen,self.ButtonColor,self.button_rectangle, border_radius= self.corners_radius)
         screen.blit(self.text_surface, self.text_rect)
-        
 
 class InputBox:
     pass
