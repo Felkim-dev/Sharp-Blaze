4e7bc71ff7864cf65ab2b177032b3fe2ac9dac3b
Felipe Quilumbango <fipojaq2005@gmail.com>
2026-03-25 16:29:54 -0500

    Input Box Without Functionaliry Implemented

--- src/client/ui/component.py
+++ src/client/ui/component.py
@@ -41,5 +41,40 @@ class Button:
         
         pygame.draw.rect(screen,self.ButtonColor,self.button_rectangle, border_radius= self.corners_radius)
         screen.blit(self.text_surface, self.text_rect)
+
 class InputBox:
-    pass
+    def __init__(
+        self,
+        Position: tuple,
+        RectangleDimension: tuple,
+        DefaultText: str
+    ):
+        # Characteristics
+        self.Position = Position
+        self.RectangleDimension = RectangleDimension
+        self.default_string = DefaultText
+
+        self.BG_COLOR = (0,0,0)
+        self.BORDER_COLOR = (255,255,255)
+        self.BORDER_SIZE = 5
+        
+        self.DEFAULT_TEXT_COLOR = (84,84,84)
+        self.button_font = pygame.font.Font(r"C:\Users\felip\OneDrive\Escritorio\SEPTIMO_SEMESTRE\Sharp-Blaze\assets\IntroRust.otf",25,)
+        self.corners_radius = 7
+
+        # Figures Instantiation
+        self.button_rectangle = pygame.Rect(Position, (RectangleDimension))
+        self.text_surface = self.button_font.render(self.default_string, True, self.DEFAULT_TEXT_COLOR)
+
+        # Text Rec Center
+        self.text_rect = self.text_surface.get_rect()
+        self.text_rect.center = self.button_rectangle.center
+
+    def draw(self,screen):
+        
+        #BGCOLOR
+        pygame.draw.rect(screen,self.BG_COLOR,self.button_rectangle, border_radius= self.corners_radius)
+        #BORDER COLOR
+        pygame.draw.rect(screen,self.BORDER_COLOR,self.button_rectangle, self.BORDER_SIZE, border_radius=self.corners_radius,)
+        
+        screen.blit(self.text_surface, self.text_rect)
