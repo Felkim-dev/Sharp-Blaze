37e19f4333ef03d7b114f6aae7e78b95f68aaa9d
Felipe Quilumbango <fipojaq2005@gmail.com>
2026-03-25 17:04:28 -0500

    First Input Box Implementation

--- src/client/ui/component.py
+++ src/client/ui/component.py
@@ -53,28 +53,48 @@ class InputBox:
         self.Position = Position
         self.RectangleDimension = RectangleDimension
         self.default_string = DefaultText
+        self.string_input = ""
 
+        # COLORS
         self.BG_COLOR = (0,0,0)
         self.BORDER_COLOR = (255,255,255)
-        self.BORDER_SIZE = 5
-        
         self.DEFAULT_TEXT_COLOR = (84,84,84)
+
+        # FONT
         self.button_font = pygame.font.Font(r"C:\Users\felip\OneDrive\Escritorio\SEPTIMO_SEMESTRE\Sharp-Blaze\assets\IntroRust.otf",25,)
+
+        # INPUT DESIGN
         self.corners_radius = 7
+        self.BORDER_SIZE = 5
 
         # Figures Instantiation
         self.button_rectangle = pygame.Rect(Position, (RectangleDimension))
-        self.text_surface = self.button_font.render(self.default_string, True, self.DEFAULT_TEXT_COLOR)
+        self.text_surface_DEFAULT = self.button_font.render(self.default_string, True, self.DEFAULT_TEXT_COLOR)
+        self.text_surface_USER = self.button_font.render(self.string_input, True, self.BORDER_COLOR)
 
         # Text Rec Center
-        self.text_rect = self.text_surface.get_rect()
+        self.text_rect = self.text_surface_DEFAULT.get_rect()
         self.text_rect.center = self.button_rectangle.center
 
+        # INITIAL STATES
+        self.is_selected = True
+
+    def change_isselected(self):
+        if self.is_selected:
+            self.is_selected = False
+        else:
+            self.is_selected = True
+
     def draw(self,screen):
-        
-        #BGCOLOR
+
+        # BGCOLOR
         pygame.draw.rect(screen,self.BG_COLOR,self.button_rectangle, border_radius= self.corners_radius)
-        #BORDER COLOR
+        # BORDER COLOR
         pygame.draw.rect(screen,self.BORDER_COLOR,self.button_rectangle, self.BORDER_SIZE, border_radius=self.corners_radius,)
-        
-        screen.blit(self.text_surface, self.text_rect)
+
+        # Draw Object
+        if self.is_selected and self.string_input:
+            self.text_surface_USER = self.button_font.render(self.string_input, True, self.BORDER_COLOR)
+            screen.blit(self.text_surface_USER, self.text_rect)
+        else:
+            screen.blit(self.text_surface_DEFAULT, self.text_rect)
