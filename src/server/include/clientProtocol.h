8898e9cd28db26274ddec7eb44c842308a0df7c8
Steve Alejandro Tene Pauta <165231304+alsteve23@users.noreply.github.com>
2026-03-30 22:31:09 -0500

    Merge pull request #4 from Felkim-dev/Steve-dev/TCP-multithread
    Steve dev/tcp multithread

---
+++ src/server/include/clientProtocol.h
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+
+namespace client_protocol
+{
+    struct InitialConnectData
+    {
+        std::string playerId;
+        std::string clientVersion;
+        bool isReady = false;
+    };
+
+    struct PlayerReadyData
+    {
+        std::string sessionId;
+    };
+    enum class ParsedMessageType
+    {
+        InitialConnect,
+        PlayerReady,
+        Unsuported
+    };
+    
+    struct ParsedMessage
+    {
+        ParsedMessageType type = ParsedMessageType::Unsuported;
+        InitialConnectData initialConnect;
+        PlayerReadyData playerReady;
+    };
+    
+    std::string BuildErrorResponse(const std::string& reason);
+    std::string BuildOkResponse();
+
+    std::string BuildQueueStatusResponse(int playerWaiting, const std::string& playerId);
+    std::string BuildMatchFoundResponse(
+        const std::string& sessionId,
+        const std::string& you,
+        const std::string& opponent);
+    
+    std::string BuildMatchStartResponse(const std::string& sessionId);
+    bool MessageFramer(
+    std::string&              carryBuffer, 
+    const char*               chunk, 
+    size_t                    chunkSize, 
+    std::vector<std::string>& outMessages);
+    
+    bool MessageProtocol(
+    const std::string&  rawMessage,
+    ParsedMessage& outMessage,
+    std::string& responseToSend);
+}
